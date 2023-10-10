6ab442e447a0c7fd2d360bab9a547b41bc67dbdb
ajay kharat <78416185+Kharat146@users.noreply.github.com>
2023-10-10 21:51:43 +0530

    email verification code added.

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import axios from "axios";
 import { Form, Input, Button, Row, Col, notification } from 'antd';
 import { UserOutlined, MailOutlined, MobileOutlined, LockOutlined } from '@ant-design/icons';
@@ -6,16 +6,59 @@ import { NavLink } from 'react-router-dom';
 import { END_POINTS } from '../../api/domain';
 import { useNavigate } from 'react-router-dom';
 import './styles.scss';
+import emailjs from '@emailjs/browser';
 
 const SignUp = () => {
   const navigate = useNavigate();
+  const form = useRef();
+
   const [isBtnLoading, setIsBtnLoading] = useState(false);
+  const [data, setdata] = useState()
+  const [emailSent, setemailSent] = useState(false)
+  const [emailVerified, setemailVerified] = useState(false)
+  const [otp, setotp] = useState()
+  const [userOtp, setUserOtp] = useState()
+  function generateRandomSixDigitNumber() {
+    return Math.floor(100000 + Math.random() * 900000);
+  }
+  const sendEmail = (e) => {
+    console.log(form.current);
+    if (data?.user_email) {
+      let cotp = generateRandomSixDigitNumber()
+      document.getElementById('message').innerHTML = cotp
+      setotp(cotp)
+      emailjs.sendForm('service_acukyoj', 'template_221j0y3', form.current, 'ZUVtc9uMSFENFge48')
+        .then((result) => {
+          console.log(result.text);
+          setemailSent(true)
+          notification.open({ message: 'otp sent' })
+        }, (error) => {
+          console.log(error.text);
+        });
+    }
+    else{
+      notification.error({message:'please enter email'})
+    }
+  };
+
+
 
   const onFinish = async (values) => {
+    setdata(values)
     console.log('Received values:', values);
-    setIsBtnLoading(true);
+  };
+
+
+  useEffect(() => {
+    console.log(userOtp,otp);
+    if (userOtp == otp && emailSent) {
+      registerUser()
+    }
+  }, [emailSent, sendEmail])
+
+  const registerUser = async () => {
     try {
-      let res = await axios.post(END_POINTS.signup, values)
+      let res = await axios.post(END_POINTS.signup, data)
       console.log(res.data);
       if (res) {
         notification.open({
@@ -25,7 +68,7 @@ const SignUp = () => {
         navigate('/login');
       }
     } catch (error) {
-      console.log(error.response.data);
+      console.log(error);
       if (error.response.data.msg) {
         notification.error({ message: error.response.data.msg })
         setIsBtnLoading(false);
@@ -35,7 +78,8 @@ const SignUp = () => {
         setIsBtnLoading(false);
       }
     }
-  };
+  }
+
 
   return (
     <div className="registration-container">
@@ -56,7 +100,7 @@ const SignUp = () => {
             </Col>
             <Col xs={24} sm={24} md={12}>
               <Form.Item
-                name="email"
+                name="user_email"
                 rules={[
                   { required: true, message: 'Please enter your email' },
                   { type: 'email', message: 'Invalid email address' },
@@ -158,13 +202,43 @@ const SignUp = () => {
               </Form.Item>
             </Col>
           </Row>
-
           <Form.Item>
+
+            <Button type="primary" onClick={sendEmail} htmlType="submit" style={{ width: '100%', margin: '1rem 0' }} loading={isBtnLoading}>
+              Verify email
+            </Button>
+            {
+              emailSent &&
+              <input type="number" placeholder='enter otp' onChange={(e) => setUserOtp(e.target.value)} />
+            }
             <Button type="primary" htmlType="submit" style={{ width: '100%' }} loading={isBtnLoading}>
               Sign-Up
             </Button>
           </Form.Item>
         </Form>
+        <form onSubmit={sendEmail} ref={form} >
+          <div>
+            <label htmlFor="user_email">Email:</label>
+            <input
+              type="email"
+              id="user_email"
+              name="user_email"
+              value={data?.user_email}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="message" >Message:</label>
+            <textarea
+              id="message"
+              name="message"
+              value={data?.message}
+              required
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+
         <div className="signup-option">
           <h3>You have an account ? <NavLink to="/login">Login Now!</NavLink></h3>
         </div>
