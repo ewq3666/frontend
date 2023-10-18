e6aeaa281249344caa5bdb5fc6039243de0a5aab
ajay kharat <78416185+Kharat146@users.noreply.github.com>
2023-10-18 09:01:44 +0530

    Wallet UI done

--- src/pages/Wallet/Wallet.jsx
+++ src/pages/Wallet/Wallet.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Button, Divider } from 'antd';
 import { IoMdClose } from "react-icons/io";
+import { FaRupeeSign } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 import { RiWallet3Line } from "react-icons/ri";
 import "./styles.scss";
 
@@ -14,8 +16,23 @@ const Wallet = () => {
                 <h1>Wallet Section</h1>
                 <div className='right-section' onClick={() => navigate('/')}> <IoMdClose /></div>
             </div>
-            <div className="wallet-content">
-                
+            <div className="wallet-section-main-container">
+
+                {/* Add Balence */}
+                <div className="balence-box">
+                    <h3>Current Balence:</h3>
+                    <h3><FaRupeeSign /> 2000</h3>
+                    <Button className="add-balence-btn"> <span className='plus-icon'>+</span>Add Amount</Button>
+                </div>
+                <Divider dashed className='wallet-divider'/>
+
+                {/* Withdraw Amount */}
+                <div className="balence-box withdrawal-box">
+                    <h3>Available Amount for Withdrawal:</h3>
+                    <h3><FaRupeeSign /> 2000</h3>
+                    <Button className="add-balence-btn"> <span className='plus-icon'>+</span>Withdrawal Amount</Button>
+                </div>
+                <Divider dashed className='wallet-divider'/>
             </div>
         </div>
     )
