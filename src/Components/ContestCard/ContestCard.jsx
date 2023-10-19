e578fd60187064cd8f34c4029d0f902e2084b953
Akshay124355 <67582907+Akshay124355@users.noreply.github.com>
2023-10-19 17:09:23 +0530

    date added

--- src/Components/ContestCard/ContestCard.jsx
+++ src/Components/ContestCard/ContestCard.jsx
@@ -3,102 +3,128 @@ import { Card, Button, Progress, Result } from 'antd';
 import './contestcard.scss';
 import { BsCurrencyRupee } from 'react-icons/bs';
 import moment from 'moment';
+import axios from 'axios';
+
 const ContestCard = () => {
 
-  const [progress, setProgress] = useState(80)
-  const twoColors = { '50%': '#108ee9', '100%': '#e31f77' };
+  const [contestData, setContestData] = useState([{
+    amount: 1000,
+    category: "NEET",
+    entryFee: 100,
+    students: 100,
+    joinStudents: 50
+  }]);
 
+  useEffect(() => {
+    axios.get('')
+      .then((response) => {
+        setContestData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching contestData:', error);
+      });
+  }, []);
+
+  const [progress, setProgress] = useState(80)
   const [isDisabled, setIsDisabled] = useState(false);
   const [result, setResult] = useState({})
-  const [showCount, setShowCount] = useState(false)
 
   console.log(result);
   const currentTime = moment();
-  const customTime = moment('11:50 PM', 'h:mm A');
-  const duration = moment.duration(customTime.diff(currentTime));
+  const targetDateTime = moment('2023-10-19 11:30 PM', 'YYYY-MM-DD HH:mm A');
+  const duration = moment.duration(targetDateTime.diff(currentTime));
+
+  const daysDiff = duration.days();
   const hoursDiff = duration.hours();
   const minutesDiff = duration.minutes();
   const secondsDiff = duration.seconds();
+
   useEffect(() => {
     const timer = setInterval(() => {
-      setResult({ hours: hoursDiff, minutes: minutesDiff, seconds: secondsDiff });
+      setResult({
+        days: daysDiff,
+        hours: hoursDiff,
+        minutes: minutesDiff,
+        seconds: secondsDiff
+      });
     }, 1000);
     return () => {
       clearInterval(timer);
     };
-  }, [customTime]);
+  }, [targetDateTime]);
+
 
   const handleCount = () => {
-    setShowCount(true);
     setIsDisabled(true);
   }
 
-  const currentDate = new Date();
-  const year = currentDate.getFullYear();
-  const month = currentDate.getMonth() + 1;
-  const day = currentDate.getDate();
-  const formattedDate = `${day}-${month}-${year}`;
 
   return (
     <>
       {result.hours > 0 || result.minutes > 0 || result.seconds > 0 ? (
         <>
-          <div className="contest-card">
-            <div className="contest-card__header">
-              <div className="contest-card__header-left">
-                <span className='contest-card__header-left__title'>Prize Pool</span>
-                <span className='contest-card__header-left__amount'><BsCurrencyRupee className='icon' />1000</span>
-              </div>
-              {showCount ?
-                <>
+          {contestData.map((values, index) => {
+            return (
+              <div className="contest-card">
+                <div className="contest-card__header">
+                  <div className="contest-card__header-left">
+                    <span className='contest-card__header-left__title'>Prize Pool</span>
+                    <span className='contest-card__header-left__amount'><BsCurrencyRupee className='icon' />{values.amount}</span>
+                  </div>
                   <div className="contest-card__header-mid">
                     <p className='countdown-box'>
-                      {showCount ? result.hours > 0 ? (
+                      {result.days === 1 ? (
+                        <p>Tomorrow</p>
+                      ) : result.days > 1 ? (
+                        <p>{targetDateTime.format('DDMMM')}</p>
+                      ) : result.days > 0 ? (
+                        <p> {result.days}d {result.hours}h {result.minutes}m {result.seconds}s</p>
+                      ) : result.hours > 0 ? (
                         <p>{result.hours}h {result.minutes}m {result.seconds}s</p>
                       ) : (
                         <p>{result.minutes}m {result.seconds}s</p>
-                      ) : ""}
+                      )}
                     </p>
-                    <p className='custom-time'>{customTime.format('h:mm A')}</p>
+                    <p className='custom-time'>{targetDateTime.format('h:mm A')}</p>
                   </div>
-                </>
-                : ""
-              }
-              <div className="contest-card__header-right">
-                <button
-                  onClick={handleCount}
-                  className={`join-button ${isDisabled ? 'disabled' : ''}`}
-                  disabled={isDisabled}>Join
-                </button>
+                  <div className="contest-card__header-right">
+                    <button
+                      onClick={handleCount}
+                      className={`join-button ${isDisabled ? 'disabled' : ''}`}
+                      disabled={isDisabled}>Join
+                    </button>
+                  </div>
+                </div>
+                <div className="contest-card__mid">
+                  <div className="contest-card__mid-category">
+                    <span>
+                      Category : {values.category}
+                    </span>
+                  </div>
+                  <div className="contest-card__mid-entryfee">
+                    <span>
+                      ENTRY FEE : <BsCurrencyRupee className='icon' />{values.entryFee}
+                    </span>
+                  </div>
+                </div>
+                <div className="contest-card__bottom">
+                  <Progress
+                    className='progress-bar'
+                    percent={progress}
+                    showInfo={false}
+                    strokeColor={{ '50%': '#108ee9', '100%': '#e31f77' }}
+                  />
+                  <div className="contest-card__bottom-title">
+                    <span className='title-first'><b>{values.joinStudents}</b> student left</span>
+                    <span className='title-second'><b>{values.students}</b>  students</span>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="contest-card__mid">
-              <div className="contest-card__mid-category">
-                <span>
-                  Category : NEET
-                </span>
-              </div>
-              <div className="contest-card__mid-entryfee">
-                <span>
-                  ENTRY FEE : <BsCurrencyRupee className='icon' />100
-                </span>
-              </div>
-            </div>
-            <div className="contest-card__bottom">
-              <Progress
-                className='progress-bar'
-                percent={progress}
-                showInfo={false}
-                strokeColor={twoColors}
-              />
-              <div className="contest-card__bottom-title">
-                <span className='title-first'><b>40</b> student left</span>
-                <span className='title-second'><b>100</b> students</span>
-              </div>
-            </div>
-          </div>
+            )
+          })
+          }
         </>
-      ) : "join next contest"}
+      ) : ""}
     </>
   );
 };
--- src/pages/MyProfile/MyProfile.jsx
+++ src/pages/MyProfile/MyProfile.jsx
@@ -5,7 +5,29 @@ const MyProfile = () => {
 
     return (
         <>
-            MyProfile page.
+            <div className="conteiner">
+                <div className="profile-card">
+                    <div className="profile-card__header">
+                        <div className="profile-card__header-logo">
+                            <p className='name'>AS</p>
+                        </div>
+                        <div className="profile-card__header-title">
+                            <p className='username'>Akshay Salve</p>
+                            <p>Level<b>:</b> 14</p>
+                        </div>
+                    </div>
+                    <div className="profile-card__footer">
+                        <div className="box">1
+                        </div>
+                        <div className="box">2
+                        </div>
+                        <div className="box">3
+                        </div>
+                        <div className="box">4
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
