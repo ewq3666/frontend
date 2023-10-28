43a466015da6deea6cfe7b1db9fe0eab6ed87afa
ajay kharat <78416185+Kharat146@users.noreply.github.com>
2023-10-28 14:52:21 +0530

    Login page changes

--- src/assets/messages.js/index.js
+++ src/assets/messages.js/index.js
@@ -1,13 +1,33 @@
-import { CheckCircleOutlined } from '@ant-design/icons';
+import { CheckCircleOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
 import { notification } from 'antd';
 
 export const loginSuccessMessage = () => {
-    notification.success({
-        message: 'Login successfully!',
-        description: 'Congratulations! You have successfully logged in to your account.',
-        icon: <CheckCircleOutlined style={{ color: '#52c41a' }} />,
-        placement: 'top',
-        duration: 1000,
-        className: "success-notification"
-      });
+  notification.success({
+    message: 'Login successfully!',
+    description: 'Congratulations! You have successfully logged in to your account.',
+    icon: <CheckCircleOutlined style={{ color: '#52c41a' }} />,
+    placement: 'top',
+    // duration: 1000,
+    className: "success-notification"
+  });
+}
+export const passwordNotMatch = () => {
+  notification.success({
+    message: 'Incorrect Password',
+    description: 'Oops! The passwords you entered do not match. Please double-check and try again.',
+    icon: <ExclamationCircleOutlined style={{ color: '#FF133D' }} />,
+    placement: 'top',
+    // duration: 1000,
+    className: "success-notification"
+  });
+}
+export const emailNotRegisterd = () => {
+  notification.success({
+    message: 'Email Not Found',
+    description: `We couldn't find an account associated with this email address. Please double-check your email or sign up if you're new to our platform.`,
+    icon: <ExclamationCircleOutlined style={{ color: '#FF133D' }} />,
+    placement: 'top',
+    // duration: 1000,
+    className: "success-notification"
+  });
 } 
