4472b09a97b1cafcfbb0c8653b16c2ab6e4b636a
ajay kharat <78416185+Kharat146@users.noreply.github.com>
2023-10-30 11:12:15 +0530

    Add Payment

--- src/pages/Wallet/Wallet.jsx
+++ src/pages/Wallet/Wallet.jsx
@@ -1,58 +1,38 @@
 import React, { useState } from 'react';
 import axios from "axios";
-import { Button, Divider } from 'antd';
+import { useSelector } from 'react-redux';
+import { Button, Divider, Form, Input, Modal } from 'antd';
 import { IoMdClose } from "react-icons/io";
 import { FaRupeeSign } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
 import { RiWallet3Line } from "react-icons/ri";
+import { handlePayment } from '../../gateway/payment';
 import "./styles.scss";
 
 const Wallet = () => {
     const navigate = useNavigate();
+    let userInfo = useSelector((state) => state.ReducerFc?.userData[0]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isAddAmount, setIsAddAmount] = useState(false);
+    const [isWithdrawRequest, setIsWithdrawRequest] = useState(false);
+    const [isBtnLoading, setIsBtnLoading] = useState(false);
 
-    const [book, setBook] = useState({
-		name: "The Fault In Our Stars",
-		author: "John Green",
-		img: "https://images-na.ssl-images-amazon.com/images/I/817tHNcyAgL.jpg",
-		price: 250,
-	});
+    const addAmountFunction = (amount, Withdraw) => {
+        setIsAddAmount(amount);
+        setIsWithdrawRequest(Withdraw);
+        setIsModalOpen(true);
+    }
 
-    const initPayment = (data) => {
-		const options = {
-			key: "rzp_test_utu5YvRUJRYmm3",
-			amount: data.amount,
-			currency: data.currency,
-			name: book.name,
-			description: "Test Transaction",
-			image: book.img,
-			order_id: data.id,
-			handler: async (response) => {
-				try {
-					const verifyUrl = "https://backendupdated.vercel.app/api/verify";
-					const { data } = await axios.post(verifyUrl, response);
-					console.log("payment",data);
-				} catch (error) {
-					console.log(error);
-				}
-			},
-			theme: {
-				color: "#3399cc",
-			},
-		};
-		const rzp1 = new window.Razorpay(options);
-		rzp1.open();
-	};
+    const onFinish = (fieldValue) => {
+        setIsBtnLoading(true);
+        console.log("fieldValue", fieldValue)
+        const amount = parseInt(fieldValue.amount);
+        console.log("userInfo",userInfo)
+        const handlePayment1 = handlePayment(amount,userInfo);
+        console.log("handlePayment1",handlePayment1)
+        setIsBtnLoading(false);
+    }
 
-	const handlePayment = async () => {
-		try {
-			const orderUrl = "https://backendupdated.vercel.app/api/orders";
-			const { data } = await axios.post(orderUrl, { amount: book.price });
-			console.log("order",data);
-			initPayment(data.data);
-		} catch (error) {
-			console.log(error);
-		}
-	};
 
     return (
         <div className="wallet-container">
@@ -67,15 +47,15 @@ const Wallet = () => {
                 <div className="balence-box">
                     <h3>Current Balence:</h3>
                     <h3><FaRupeeSign /> 2000</h3>
-                    <Button 
+                    <Button
                         className="add-balence-btn"
-                        onClick={handlePayment}
-                    > 
+                        onClick={() => addAmountFunction(true, false)}
+                    >
                         <span className='plus-icon'>+</span>
                         Add Amount
                     </Button>
                 </div>
-                <Divider dashed className='wallet-divider'/>
+                <Divider dashed className='wallet-divider' />
 
                 {/* Withdraw Amount */}
                 <div className="balence-box withdrawal-box">
@@ -83,14 +63,64 @@ const Wallet = () => {
                     <h3><FaRupeeSign /> 2000</h3>
                     <Button
                         className="Withdraw-btn"
-                        // onClick={() => addAmountFunction(false, true)}
+                        onClick={() => addAmountFunction(false, true)}
                     >
                         <span className='plus-icon'><FaRupeeSign /></span>
                         Withdrawal Amount
                     </Button>
                 </div>
-                <Divider dashed className='wallet-divider'/>
+                <Divider dashed className='wallet-divider' />
             </div>
+            <Modal
+                title={isAddAmount ? "Add Amount" : isWithdrawRequest ? "Withdrawal Request" : ""}
+                open={isModalOpen}
+                onCancel={() => setIsModalOpen(false)}
+                className='add-amount-modal'
+            >
+                <div className="add-amount-box">
+                    {isAddAmount ?
+                        <div className="add-amount-form">
+                            <Form
+                                name="amountForm"
+                                style={{
+                                    maxWidth: 600,
+                                }}
+                                onFinish={onFinish}
+                                autoComplete="off"
+                            >
+                                <Form.Item
+                                    label="Add credit to your account to play more quizzes:"
+                                    name="amount"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: 'Please enter amount',
+                                        },
+                                    ]}
+                                    className='common-input'
+                                >
+                                    <input type="number" placeholder='Enter amount' />
+                                </Form.Item>
+                                <div className="button-box">
+                                    <Button htmlType="submit" className='common-blue-button' loading={isBtnLoading}>
+                                        Add Amount
+                                    </Button>
+                                </div>
+                            </Form>
+                        </div>
+                        : isWithdrawRequest ?
+                            <div className='withdrawal-request'>
+                                <p>You're just one step away from cashing in your earnings! Please complete the withdrawal request form, and we'll process your funds shortly</p>
+                                <div className="button-box">
+                                    <Button htmlType="submit" className='common-yellow-button' >
+                                        Request For Withdrawal
+                                    </Button>
+                                </div>
+                            </div>
+                            : ""
+                    }
+                </div>
+            </Modal>
         </div>
     )
 }
