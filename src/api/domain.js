243a2e7869ee35649666720735045b84ba9e11a5
ewq3666 <145737607+ewq3666@users.noreply.github.com>
2023-11-21 17:35:25 +0530

    Add files via upload

--- src/api/domain.js
+++ src/api/domain.js
@@ -1,14 +1,15 @@
 const api = process.env.REACT_APP_API
 
-export const END_POINTS={
-     users  :`${api}`,
-     signup : `${api}/signup`,
-     login  :`${api}/login`,
-     handleDuplicateEmail  :`${api}/email`,
-     userInfo  :`${api}/user`,
-     verify  :`${api}/verify`,
-     orders  :`${api}/orders`,
-     addmoney  :`${api}/addmoney`,
-     contest  :`${api}/admin/contest`,
-     getBalence  :`${api}/balance/`,
+export const END_POINTS = {
+     users: `${api}`,
+     signup: `${api}/signup`,
+     login: `${api}/login`,
+     handleDuplicateEmail: `${api}/email`,
+     userInfo: `${api}/user`,
+     verify: `${api}/verify`,
+     orders: `${api}/orders`,
+     addmoney: `${api}/addmoney`,
+     contest: `${api}/admin/contest`,
+     getBalence: `${api}/balance/`,
+     widthdrawRequest: `${api}/withdraw-request`
 }
